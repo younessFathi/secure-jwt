95fa1fd7769f4903616de8884775235d0dabc908
unknown <theyounessfathi@gmail.com>
2020-01-12 13:51:20 +0100

    [ adding building feature ]

--- src/test/java/com/security/controller/HelloWorldControllerTest.java
+++ src/test/java/com/security/controller/HelloWorldControllerTest.java
@@ -4,6 +4,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,10 +20,18 @@ public class HelloWorldControllerTest {
 
 	@Autowired
 	private MockMvc mvc;
+	
+	private String jwtToken;
+	
+	@Before
+	public void init() {
+		this.jwtToken = "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ5b3VuZXNzIiwiZXhwIjoxNTc4ODUwOTI2LCJpYXQiOjE1Nzg4MzI5MjZ9.TaLPKR70z_x1MmWzTyNI8I0L3jBwte-p5m3SVZ4BPEADXpU3t3z4jfGirkBJnVIlKDcVh6PUMzrdjOMmrpuvGA";
+	}
 
 	@Test
 	public void helloWorldTest() throws Exception {
-		this.mvc.perform(get("/hello")).andExpect(status().isOk()).andDo(print());
+		this.mvc.perform(get("/hello").header("Authorization" , jwtToken))
+				.andExpect(status().isOk()).andDo(print());
 	}
 
 }
